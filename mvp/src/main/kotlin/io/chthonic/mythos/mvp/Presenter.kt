9641877b1e37c00642f9ba805b810c85fed9402f
jhavatar <jhdevaal@gmail.com>
2016-04-04 06:27:12 +0200

    updated kotlin version initial documentaion

--- mvp/src/main/kotlin/io/chthonic/mythos/mvp/Presenter.kt
+++ mvp/src/main/kotlin/io/chthonic/mythos/mvp/Presenter.kt
@@ -9,54 +9,107 @@ import java.lang.ref.WeakReference
  */
 abstract class Presenter<V> where V : Vu {
 
+    /** Memory leak safe reference to attached Vu */
     private var _vuRef: WeakReference<V>? = null;
+
+    /** Memory leak safe reference activity. */
     private var _activityRef: WeakReference<Activity>? = null;
+
+    /** True if Presenter is attached */
     var attached: Boolean = true;
 
+    /** Attach Vu to Presenter.
+     *
+     * @param vu VU thatis being attached.
+     */
     open fun attachVu(vu: V) {
         this._vuRef = WeakReference<V>(vu);
     }
+
+    /**
+     * Detach Vu from Presenter
+     */
     open fun detachVu() {
         this._vuRef = null;
     }
+
+    /**
+     * Return attached Vu.
+     */
     fun getVu(): V? {
         return _vuRef?.get();
     }
+
+    /**
+     * Return true if a Vu is attached.
+     */
     fun hasVu(): Boolean {
         return (_vuRef != null) && (getVu() != null);
     }
 
+    /**
+     * Return true if the activity is still available.
+     */
     fun hasActivity(): Boolean {
         return (_activityRef != null) && (getActivity() != null);
     }
+
+    /** Return activity. */
     fun getActivity(): Activity? {
         return _activityRef?.get();
     }
 
+    /**
+     * Initialize Presenter. Call after passed parameters are initialized.
+     *
+     * @param activity Activity that the PResenter belongs to.
+     * @param args Arguments passed to presented object.
+     * @param inState Data of Presenter's instance last saved in onSaveState call.
+     */
     open fun initialize(activity: Activity, args: Bundle, inState: Bundle?) {
         _activityRef = WeakReference<Activity>(activity);
     }
 
+    /**
+     * Called when Vu is to be displayed.
+     */
     open fun onStartVu() {
 
     }
 
+    /**
+     * Called when Vu is moved to screen's foreground.
+     */
     open fun onResumeVu() {
 
     }
 
+    /**
+     * Called when Vu is moved from screen's foreground to background.
+     */
     open fun onPauseVu() {
 
     }
 
+    /**
+     * Called when Vu is no longer visible.
+     */
     open fun onStopVu() {
 
     }
 
+    /**
+     * Called when presented object is being destroyed. Perform final Presenter cleanup.
+     */
     open fun onDestroy() {
         attached = false;
     }
 
+    /**
+     * Called to allow saving of Presenter's data when presented object is about to be killed.
+     *
+     * @param outState Put Presenter's data to be saved in outState. Data will be available in initialize() call.
+     */
     open fun onSaveState(outState: Bundle) {
 
     }
--- mvp/src/main/kotlin/io/chthonic/mythos/mvp/Vu.kt
+++ mvp/src/main/kotlin/io/chthonic/mythos/mvp/Vu.kt
@@ -10,27 +10,48 @@ import android.view.ViewGroup
  * Created by jhavatar on 3/3/2016.
  */
 abstract class Vu(inflater: LayoutInflater,
+                  /** Activity that Vu rootView belongs to. */
                   val activity: Activity,
+                  /** Fragment that Vu's rootView is a child of (Optional). */
                   var fragment: Fragment? = null,
+                  /** ViewGroup that is the direct parent to Vu's rootView (Optional). */
                   var parentView: ViewGroup? = null) {
 
+    /**
+     * The root of the views that the Vu manages. Can be lazily created as soon as Vu is created.
+     */
     val rootView : View by lazy {
         createRootView(inflater);
     }
 
+    /** True if Vu is attached to a Presenter */
     var attached: Boolean = true;
+
+    /** True if Vu is in screen's foreground */
     var focused: Boolean = false;
 
+    /**
+     * Called when Vu is moves to and from the screen's foreground.
+     */
     fun onFocusedChanged(hasFocus: Boolean) {
         focused = true;
     }
 
+    /**
+     * Called after Vu is detached from Presenter its rootView will no longer be used. Perform any final Vu cleanup.
+     */
     open fun onDestroy() {
         attached = false;
     }
 
+    /**
+     * Return id of layout resource file that is the rootView of the Vu.
+     */
     abstract fun getRootViewLayoutId() : Int;
 
+    /**
+     * Create rootView by inflating getRootViewLayoutId()'s layout.
+     */
     protected fun createRootView(inflater: LayoutInflater) : View {
         if (parentView != null) {
             return inflater.inflate(getRootViewLayoutId(), parentView, false);
