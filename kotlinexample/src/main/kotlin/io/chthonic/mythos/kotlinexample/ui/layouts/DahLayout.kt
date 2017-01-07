9aa5a045581a16c8d749abe7eb9b38020de8a141
jhavatar <jhdevaal@gmail.com>
2017-01-07 11:57:34 +0200

    more kotlinexample code cleanup

--- kotlinexample/src/main/kotlin/io/chthonic/mythos/kotlinexample/ui/layouts/DahLayout.kt
+++ kotlinexample/src/main/kotlin/io/chthonic/mythos/kotlinexample/ui/layouts/DahLayout.kt
@@ -1,13 +1,9 @@
 package io.chthonic.mythos.kotlinexample.ui.layouts
 
-import android.app.Activity
 import android.content.Context
 import android.util.AttributeSet
-import android.view.LayoutInflater
-import android.view.ViewGroup
 import io.chthonic.mythos.kotlinexample.ui.presenters.DahPresenter
 import io.chthonic.mythos.kotlinexample.ui.vus.DahVu
-import io.chthonic.mythos.mvp.FragmentWrapper
 import io.chthonic.mythos.mvp.MVPDispatcher
 import io.chthonic.mythos.mvp.MVPLayout
 import io.chthonic.mythos.mvp.PresenterCacheBasic
